090e4d87444ca3fbb29396f9f7e3a34a773f4cb1
promptc3 <promptc3.0@gmail.com>
2017-03-20 21:18:17 +0530

    add uploader

--- app/controllers/paws_controller.rb
+++ app/controllers/paws_controller.rb
@@ -69,6 +69,6 @@ class PawsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def paw_params
-      params.fetch(:paw, {})
+      params.require(:paw).permit(:avatar)
     end
 end
--- app/models/paw.rb
+++ app/models/paw.rb
@@ -1,2 +1,6 @@
 class Paw < ApplicationRecord
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>",large: '1000x1000>' }
+    validates_attachment :avatar, presence: true,
+    content_type: { content_type: "image/jpeg" },
+    size: { in: 0..10.megabytes }
 end
