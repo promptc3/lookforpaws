1d23b5e001041586a45583170fe4a32038e2b5e7
promptc3 <promptc3.0@gmail.com>
2017-03-20 23:20:59 +0530

    add validation for image

--- app/models/paw.rb
+++ app/models/paw.rb
@@ -2,5 +2,5 @@ class Paw < ApplicationRecord
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>",large: '1000x1000>' }
     validates_attachment :avatar, presence: true,
     content_type: { content_type: "image/jpeg" },
-    size: { in: 0..10.megabytes }
+    size: { in: 0..10.kilobytes }
 end
