60003859b27af232e6eaf64ac3cec0edfb9cb3c6
promptc3 <promptc3.0@gmail.com>
2017-03-22 14:20:12 +0530

    updated validation

--- app/models/paw.rb
+++ app/models/paw.rb
@@ -1,6 +1,6 @@
 class Paw < ApplicationRecord
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>",large: '1000x1000>' }
+	has_attached_file :avatar, styles: { medium: "600x600>", thumb: "100x100>",large: '1000x1000>' }
     validates_attachment :avatar, presence: true,
-    content_type: { content_type: "image/jpeg" },
-    size: { in: 0..10.kilobytes }
+    content_type: { content_type: ["image/jpeg", "image/png", "image/jpg"] },
+    size: {in: 0..500.kilobytes }
 end
