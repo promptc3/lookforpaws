9cfb0c3bb98080fee798751d266b63345874c8e7
promptc3 <promptc3.0@gmail.com>
2017-03-23 02:11:28 +0530

    add comment

--- app/models/paw.rb
+++ app/models/paw.rb
@@ -1,4 +1,6 @@
 class Paw < ApplicationRecord
+     belongs_to :about	    
+	    #paperclip validations
 	has_attached_file :avatar, styles: { medium: "600x600>", thumb: "100x100>",large: '1000x1000>' }
     validates_attachment :avatar, presence: true,
     content_type: { content_type: ["image/jpeg", "image/png", "image/jpg"] },
