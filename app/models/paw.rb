598e3624fe35eadfe0b22473c3f8fcd78d308a7d
promptc3 <promptc3.0@gmail.com>
2017-03-24 23:20:43 +0530

    fix validations

--- app/models/paw.rb
+++ app/models/paw.rb
@@ -1,8 +1,11 @@
 class Paw < ApplicationRecord
-     belongs_to :about	    
-	    #paperclip validations
+	#paws features validation
+	validates :name, presence: true
+    #paperclip validations
 	has_attached_file :avatar, styles: { medium: "600x600>", thumb: "100x100>",large: '1000x1000>' }
-    validates_attachment :avatar, presence: true,
-    content_type: { content_type: ["image/jpeg", "image/png", "image/jpg"] },
-    size: {in: 0..500.kilobytes }
+    validates_with AttachmentPresenceValidator, :attributes => :avatar
+    validates_with AttachmentSizeValidator, :attributes => :avatar, less_than: 1.megabytes
+    validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+    validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+
 end
