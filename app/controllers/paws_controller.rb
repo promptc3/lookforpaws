48ff92c7035b1794d96ec879666b94496079b0c3
promptc3 <promptc3.0@gmail.com>
2017-03-24 23:20:13 +0530

    POST action bug fix

--- app/controllers/paws_controller.rb
+++ app/controllers/paws_controller.rb
@@ -25,11 +25,11 @@ class PawsController < ApplicationController
   # POST /paws.json
   def create
     @paw = Paw.new(paw_params)
-    
     respond_to do |format|
       if @paw.save
         format.html { redirect_to @paw, notice: 'Paw was successfully created.' }
         format.json { render :show, status: :created, location: @paw }
+        
       else
         format.html { render :new }
         format.json { render json: @paw.errors, status: :unprocessable_entity }
@@ -69,6 +69,6 @@ class PawsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def paw_params
-       params.require(:paw).permit(:avatar,:name,:gender,:breed,:dob) if params[:avatar]
+       params.require(:paw).permit(:avatar,:name, :gender, :breed, :dob)
     end
 end
