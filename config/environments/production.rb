f8fb1aca1c4ba80b1b0e0309b7173cf9e5e8473b
promptc3 <promptc3.0@gmail.com>
2017-04-02 23:26:04 +0530

    update host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,8 @@ Rails.application.configure do
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION')
-    }
+    },
+    s3_host_name: 's3-ap-south-1.amazonaws.com',
+    url: ":s3_domain_url"
 }
 end
